6156ed1bf3be2403b6b828b037d738f9ad328717
Nikolay Latin <gs3d@yandex.ru>
2024-01-14 10:36:35 +0300

    review

--- ShareFileCloud/Controllers/AuthController.cs
+++ ShareFileCloud/Controllers/AuthController.cs
@@ -1,8 +1,12 @@
 ﻿using DTO.Models;
 using DTO.Repository;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using ShareFileCloud.Services.Auth;
 using ShareFileCloud.SimpleModels.Auth;
+using System.Security.Claims;
 
 namespace ShareFileCloud.Controllers
 {
@@ -26,20 +30,21 @@ namespace ShareFileCloud.Controllers
 
 #if DEBUG
         [HttpPost("debug-create-user")]
-        public async Task<ActionResult<ApplicationUserDTO>> CreateUserAsync([FromBody] Login model)
+        public async Task<ActionResult<ApplicationUserDTO>> CreateUserAsync([FromBody] Login login)
         {
             if (!ModelState.IsValid)
                 return BadRequest();
             var user = await _usersRepository.CreateAsync(new DTO.Models.ApplicationUserDTO()
             {
-                UserName = model.UserName,
-                PasswordHash = _authService.HashPassword(model.Password ?? string.Empty)
+                UserName = login.UserName,
+                PasswordHash = _authService.HashPassword(login.Password ?? string.Empty)
             }, string.Empty);
             return Ok(user);
         }
 #endif
 
 
+        /*
         [HttpPost("login")]
         public async Task<IActionResult> LoginAsync([FromBody] Login model)
         {
@@ -48,6 +53,49 @@ namespace ShareFileCloud.Controllers
             var answer = await _authService.LoginAsync(model);
             return answer.State ? Ok(answer) : BadRequest(answer);
         }
+
+
+        [HttpPost("logout")]
+        [Authorize]
+        public ActionResult Logout()
+        {
+            var userName = User.Identity?.Name!;
+            jwtAuthManager.RemoveRefreshTokenByUserName(userName);
+            logger.LogInformation("User [{userName}] logged out the system.", userName);
+            return Ok();
+        }
+
+        [HttpPost("refresh-token")]
+        [Authorize]
+        public async Task<ActionResult> RefreshToken([FromBody] RefreshTokenRequest request)
+        {
+            try
+            {
+                var userName = User.Identity?.Name!;
+                logger.LogInformation("User [{userName}] is trying to refresh JWT token.", userName);
+
+                if (string.IsNullOrWhiteSpace(request.RefreshToken))
+                {
+                    return Unauthorized();
+                }
+
+                var accessToken = await HttpContext.GetTokenAsync("Bearer", "access_token");
+                var jwtResult = jwtAuthManager.Refresh(request.RefreshToken, accessToken ?? string.Empty, DateTime.Now);
+                logger.LogInformation("User [{userName}] has refreshed JWT token.", userName);
+                return Ok(new LoginResult
+                {
+                    UserName = userName,
+                    Role = User.FindFirst(ClaimTypes.Role)?.Value ?? string.Empty,
+                    AccessToken = jwtResult.AccessToken,
+                    RefreshToken = jwtResult.RefreshToken.TokenString
+                });
+            }
+            catch (SecurityTokenException e)
+            {
+                return Unauthorized(e.Message); // return 401 so that the client side can redirect the user to login page
+            }
+        }*/
+
 
 
         /*
